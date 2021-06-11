66c565e8f4ee30880323781cf20021f9434351f1
Ashley <ashley.honn@gmail.com>
2021-06-10 21:11:52 -0700

    committt

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Platform, TextInput, Text, View } from "react-native";
 
 export default function App() {
+  const [num1, onChangeNum1] = React.useState(0);
+  const [num2, onChangeNum2] = React.useState(0);
+  const [message, setMessage] = React.useState('result');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.boxTextHead}>Midterm Exam</Text>
+        <Text style={styles.boxTextBody}>Compare two numbers</Text>
+      </View>
+      
+      <Text style={styles.boxTextNum}>Number 1:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNum1}
+        />
+      <Text style={styles.boxTextNum}>Number 2:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNum2}
+        />
+
+        <Button title="COMPARE" 
+                color="blue"
+                onPress={() => Message(useState())} />
+        <StatusBar style="auto" />
+        <Text>{message} </Text>
     </View>
   );
 }
@@ -14,8 +38,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: "column",
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    ...Platform.select ({
+       ios: {paddingTop: 20},
+       android: {paddingTop: StatusBar.currentHeight}
+    })
   },
+
+  box: {
+    height: 200,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "center",
+    backgroundColor: "teal"
+  },
+
+  boxTextHead: {
+    fontSize: 30,
+    color: "white",
+  },
+
 });
+
+//*this is the end
